d6e2e86c9ba007d5c6ba15362b0534228a36e5a2
intrepiditee <31870027+intrepiditee@users.noreply.github.com>
2020-06-26 16:27:45 +0000

    container

--- import-automation/task.sh
+++ import-automation/task.sh
@@ -1,3 +1,3 @@
-pip install -U pip setuptools 
-pip install -r import-automation/requirements.txt
+pip3 install -U pip setuptools 
+pip3 install -r import-automation/requirements.txt
 python3 import-automation/app.py $@
