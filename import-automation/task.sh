b30b53d52b0d9d1d00b63b4d6837f4b88fbd99d1
intrepiditee <31870027+intrepiditee@users.noreply.github.com>
2020-06-26 18:59:17 +0000

    pip

--- import-automation/task.sh
+++ import-automation/task.sh
@@ -1,8 +1,5 @@
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python3 get-pip.py
-# add-apt-repository universe
-# apt update
-# apt install python3-pip
-python3 -m pip install -U pip setuptools 
+# python3 -m pip install -U pip setuptools 
 python3 -m pip install -r import-automation/requirements.txt
 python3 import-automation/app.py $@
