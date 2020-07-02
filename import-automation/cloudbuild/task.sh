e35325682540331dbad4a2a44d8b58007632af08
intrepiditee <31870027+intrepiditee@users.noreply.github.com>
2020-07-02 16:13:03 +0000

    test

--- import-automation/cloudbuild/task.sh
+++ import-automation/cloudbuild/task.sh
@@ -1,4 +1,4 @@
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python3 get-pip.py
 python3 -m pip install -r import-automation/requirements.txt
-python3 import-automation/app.py $@
+python3 import-automation/create_app.py
